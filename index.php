b69911c603b373b45802c9d62f4eaf1c500187fd
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-06-23 13:44:25 +0100

    added base logic for 3 and 4 players

--- index.php
+++ index.php
@@ -39,29 +39,46 @@ function newHand(Deck $deck, int $player) {
     return $score;
 }
 
-echo "<div class='playerCountSelect'>
+?>
+
+<div class='playerCountSelect'>
 <h2 class='playerCountTitle'>Number of Players</h2>
-<button>2</button>
-<button>3</button>
-<button>4</button>
-</div>";
+<form method="get">
+    <button type="submit" name="players" value="2">2</button>
+    <button type="submit" name="players" value="3">3</button>
+    <button type="submit" name="players" value="4">4</button>
+</form>
+</div>
 
+<?php
 
-$scorePlayer1 = newHand($cardDeck, 1);
-$scorePlayer2 = newHand($cardDeck, 2);
+if (isset($_GET['players']) && $_GET['players'] == 2) {
+    $scorePlayer1 = newHand($cardDeck, 1);
+    $scorePlayer2 = newHand($cardDeck, 2);
 
-if ($scorePlayer1 > 21 && $scorePlayer2 > 21) {
-    echo "</br> <h2 class='outcome'>It's a draw, both players are bust!</h2>";
-} elseif ($scorePlayer1 > 21) {
-    echo "</br> <h2 class='outcome'>Player 2 wins! Player 1 is bust.</h2>";
-} elseif ($scorePlayer2 > 21) {
-    echo "</br> <h2 class='outcome'>Player 1 wins! Player 2 is bust.</h2>";
-} elseif ($scorePlayer1 == $scorePlayer2) {
-    echo "</br> <h2 class='outcome'>It is a draw</h2>";
-} elseif ($scorePlayer1 > $scorePlayer2) {
-    echo "</br> <h2 class='outcome'>Player 1 wins!</h2>";
-} else {
-    echo "</br> <h2 class='outcome'>Player 2 wins!</h2>";
+    if ($scorePlayer1 > 21 && $scorePlayer2 > 21) {
+        echo "</br> <h2 class='outcome'>It's a draw, both players are bust!</h2>";
+    } elseif ($scorePlayer1 > 21) {
+        echo "</br> <h2 class='outcome'>Player 2 wins! Player 1 is bust.</h2>";
+    } elseif ($scorePlayer2 > 21) {
+        echo "</br> <h2 class='outcome'>Player 1 wins! Player 2 is bust.</h2>";
+    } elseif ($scorePlayer1 == $scorePlayer2) {
+        echo "</br> <h2 class='outcome'>It is a draw</h2>";
+    } elseif ($scorePlayer1 > $scorePlayer2) {
+        echo "</br> <h2 class='outcome'>Player 1 wins!</h2>";
+    } else {
+        echo "</br> <h2 class='outcome'>Player 2 wins!</h2>";
+    }
+} elseif (isset($_GET['players']) && $_GET['players'] == 3) {
+    $scorePlayer1 = newHand($cardDeck, 1);
+    $scorePlayer2 = newHand($cardDeck, 2);
+    $scorePlayer3 = newHand($cardDeck, 3);
+
+} elseif (isset($_GET['players']) && $_GET['players'] == 4) {
+    $scorePlayer1 = newHand($cardDeck, 1);
+    $scorePlayer2 = newHand($cardDeck, 2);
+    $scorePlayer3 = newHand($cardDeck, 3);
+    $scorePlayer4 = newHand($cardDeck, 4);
 }
 
 echo '
