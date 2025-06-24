6b36a9c9c3f3b8ab134866a0f43f1492159776c7
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-06-24 10:34:59 +0100

    added logic for player 1 and 2 losing and others drawing

--- index.php
+++ index.php
@@ -96,6 +96,10 @@ if (isset($_GET['players']) && $_GET['players'] == 2) {
         echo "</br> <h2 class='outcome'>Player 3 wins!</h2>";
     } else if ($scorePlayer1 == $scorePlayer2 && $scorePlayer1 <= 21 && ($scorePlayer3 > 21 || $scorePlayer3 < $scorePlayer1)) {
         echo "</br> <h2 class='outcome'>Player 1 and Player 2 draw! Player 3 loses.</h2>";
+    } else if ($scorePlayer1 == $scorePlayer3 && $scorePlayer1 <= 21 && ($scorePlayer2 > 21 || $scorePlayer2 < $scorePlayer1)) {
+        echo "</br> <h2 class='outcome'>Player 1 and Player 3 draw! Player 2 loses.</h2>";
+    } else if ($scorePlayer2 == $scorePlayer3 && $scorePlayer2 <= 21 && ($scorePlayer1 > 21 || $scorePlayer1 < $scorePlayer2)) {
+        echo "</br> <h2 class='outcome'>Player 2 and Player 3 draw! Player 1 loses.</h2>";
     }
 
 } elseif (isset($_GET['players']) && $_GET['players'] == 4) {
